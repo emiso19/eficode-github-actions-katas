488e42b2fdc8fe1a9d55a978002854db59f3f17b
Emil Klindt Sørensen <emiso19@student.sdu.dk>
2023-08-16 11:17:16 +0000

    Updated component and performance test

--- ci/performance-test.sh
+++ ci/performance-test.sh
@@ -1,3 +1,5 @@
-#! /bin/bash
-[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker_username=$DockerRepo docker-compose -f performance-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
+#!/bin/bash
+docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
+[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username_lower}/"
+export DockerRepo
+docker-compose -f performance-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
